dbe4b8ce89faef998e5fb763747a8de34cd0b8ed
Ulf Luecking <ulf.luecking@gmail.com>
2024-06-25 00:48:36 +0200

    Added sub routine.

--- aes256-crypt-cbc-dec.pl
+++ aes256-crypt-cbc-dec.pl
@@ -16,33 +16,38 @@ GetOptions(
 
 die "$0 requires the file argument (--file)\n" unless $file;
 
-my $obj = do {
-    local $/ = undef;
-    open my $fh, "<:encoding(UTF-8)", $file or die "could not open $file: $!\n";
-    <$fh>;
-};
-
-my $decoded = decode_base64($obj);
-
 my $password = prompt 'Enter your password:', -echo=>'*';
 
-# my $plaintext = '{"parameter_a": "parameter_a_value","parameter_b": "parameter_b_value"}';
-# my $password = 'observing congrats exhume diabolic';
-my $cipher = Crypt::CBC->new(
+my $json_out = aes_cbc_dec($file, $password);
+
+my $parameter_a = $json_out->{'parameter_a'};
+my $parameter_b = $json_out->{'parameter_b'};
+print "result: $parameter_a, $parameter_b\n";
+
+sub aes_cbc_dec {
+    my ($file, $password, $refList) = @_;
+
+    my $obj = do {
+        local $/ = undef;
+        open my $fh, "<:encoding(UTF-8)", $file or die "could not open $file: $!\n";
+        <$fh>;
+    };
+
+    my $decoded = decode_base64($obj);
+    my $cipher = Crypt::CBC->new(
         -key    => $password,
         -cipher => "Crypt::OpenSSL::AES",
         -pbkdf  => 'pbkdf2',
-);
+    );
 
-# my $encrypted = $cipher->encrypt($plaintext);
-my $decrypted = $cipher->decrypt($decoded);
+    my $decrypted = $cipher->decrypt($decoded);
+    my $json_out = eval { decode_json($decrypted) };
 
-my $json_out = eval { decode_json($decrypted) };
-if ($@) {
-    print "decode_json failed, invalid json. error:$@\n";
-}
-else {
-    my $parameter_a = $json_out->{'parameter_a'};
-    my $parameter_b = $json_out->{'parameter_b'};
-    print "result: $parameter_a, $parameter_b\n";
+    if ($@) {
+        print "wrong password, decode_json failed, invalid json. error: $@\n";
+        exit 1;
+    }
+    else {
+        return $json_out;
+    }
 }
